97e08e2ac02f994891bf2a3695214f56cdb91877
Rammah Karpous <rammahkarpous@outlook.com>
2024-12-05 18:04:48 +0100

    add like functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 const listHeadings = document.querySelectorAll('footer section div h4');
 
 listHeadings.forEach((heading) => {
-  const list = heading.nextElementSibling;
+	const list = heading.nextElementSibling;
 
-  heading.addEventListener('click', () => {
-    list.classList.toggle('show');
-  });
+	heading.addEventListener('click', () => {
+		list.classList.toggle('show');
+	});
 });
+
+const heart = document.querySelector('.heart-count');
+const button = document.querySelectorAll('button');
+
+button.forEach((button) => {
+	button.addEventListener('click', () => {
+		if (button.classList.contains('liked')) {
+			heart.innerText = parseInt(heart.innerText) - 1;
+			button.classList.remove('liked');
+		} else {
+			heart.innerText = parseInt(heart.innerText) + 1;
+			button.classList.add('liked');
+		}
+	});
+})
+heart.innerText = '0';
